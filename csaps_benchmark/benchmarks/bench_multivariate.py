721ae09167567520bed7152646aae11995b11470
Eugene Prilepin <esp.home@gmail.com>
2020-01-10 08:50:37 +0300

    Add multivariate.evaluate_spline benchmark

--- csaps_benchmark/benchmarks/bench_multivariate.py
+++ csaps_benchmark/benchmarks/bench_multivariate.py
@@ -9,3 +9,12 @@ from csaps import csaps
 def bench_make_spline(benchmark, multivariate_data, ndim, size):
     x, y = multivariate_data(ndim=ndim, size=size)
     benchmark(csaps, x, y)
+
+
+@pytest.mark.benchmark(group='multivariate.evaluate_spline')
+def bench_evaluate_spline(benchmark, multivariate_data, output_data_sites, ndim, input_size, output_size):
+    x, y = multivariate_data(ndim=ndim, size=input_size)
+    xi = output_data_sites(x, size=output_size)
+    spline = csaps(x, y)
+
+    benchmark(spline, xi)

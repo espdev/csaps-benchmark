2b91a9591f9cf66d0ebd7f920182351521257a39
Eugene Prilepin <esp.home@gmail.com>
2020-01-10 08:34:13 +0300

    code format

--- csaps_benchmark/benchmarks/bench_multivariate.py
+++ csaps_benchmark/benchmarks/bench_multivariate.py
@@ -5,9 +5,7 @@ import pytest
 from csaps import csaps
 
 
-@pytest.mark.benchmark(
-    group='multivariate.make_spline',
-)
+@pytest.mark.benchmark(group='multivariate.make_spline')
 def bench_make_spline(benchmark, multivariate_data, ndim, size):
     x, y = multivariate_data(ndim=ndim, size=size)
     benchmark(csaps, x, y)

b8ee02fb7a88d343c81ae078b8db9f9c0ddd2065
Eugene Prilepin <prilepin@smedx.com>
2020-01-23 17:52:34 +0300

    add ndgrid.evaluate_spline benchmark

--- csaps_benchmark/benchmarks/bench_ndgrid.py
+++ csaps_benchmark/benchmarks/bench_ndgrid.py
@@ -10,3 +10,13 @@ def bench_make_spline(benchmark, ndgrid_data, ndim, size):
     shape = [size] * ndim
     x, y = ndgrid_data(shape=shape)
     benchmark(csaps, x, y)
+
+
+@pytest.mark.benchmark(group='ndgrid.evaluate_spline')
+def bench_evaluate_spline(benchmark, ndgrid_data, output_data_sites, ndim, input_size, output_size):
+    shape = [input_size] * ndim
+    x, y = ndgrid_data(shape=shape)
+    xi = output_data_sites(x, output_size)
+
+    spline = csaps(x, y)
+    benchmark(spline, xi)
